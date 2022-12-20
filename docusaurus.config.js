78808e6e2f7995c40da448d34d63e1a64c5c928f
atuonufure <atuonufure@gmail.com>
2022-12-20 13:17:28 +0100

    Update config and remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "beda-software", // Usually your GitHub org/user name.
+  projectName: "aidbox-develop-starter-guide", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -83,7 +83,7 @@ const config = {
               },
               {
                 label: "aidbox-react",
-                to: "/docs/aidbox-react",
+                to: "/docs/category/aidbox-react",
               },
             ],
           },
