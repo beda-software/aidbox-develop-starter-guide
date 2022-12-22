14de672925a52d11f35d826a2f2e305501e171b4
atuonufure <atuonufure@gmail.com>
2022-12-22 19:39:06 +0100

    Update config and readme

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
     title: 'aidbox-develop-starter-guide ',
     tagline: 'work in progress',
     url: 'https://your-docusaurus-test-site.com',
-    baseUrl: '/',
+    baseUrl: '/aidbox-develop-starter-guide/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
